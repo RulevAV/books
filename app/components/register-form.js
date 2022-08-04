949c961d1587eefc0b40c6daa7a8b6ac46dad3fa
RulevAV <rulandrei99@mail.ru>
2022-08-05 04:02:52 +0700

    CAPTCHA

--- app/components/register-form.js
+++ app/components/register-form.js
@@ -1,6 +1,11 @@
 import Component from '@ember/component';
+import ENV from 'books/config/environment';
 
 export default Component.extend({
+    iAmRobot: true,
+    reset: false,
+    siteKeyCaptcha:ENV['ember-cli-google'].recaptcha.siteKey,
+
     actions: {
         async saveReview(e) {
             e.preventDefault();
@@ -8,6 +13,19 @@ export default Component.extend({
                 email: this.get('email'),
                 password: this.get('password'),
             });
+        },
+
+        async verified(key) {
+            try {
+                const { success } = await (await fetch(`${ENV.backendUrl}/recaptcha?key=${key}`)).json();
+                this.set('iAmRobot', !success);
+            } catch (error) {
+                this.set('reset', true);
+            }
+        },
+
+        expired() {
+            this.set('iAmRobot', true);
         }
-    }
+    },
 });
